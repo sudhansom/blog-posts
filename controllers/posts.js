20299e22edfc9b67d503b3c2864fdceb5dfaa27c
bhuwan <bkspoudel@gmail.com>
2023-11-05 15:27:44 +0100

    fix all crud operations

--- controllers/posts.js
+++ controllers/posts.js
@@ -25,5 +25,28 @@ const createPost = async (req, res, next) => {
   }
 };
 
+const deletePost = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const post = await Post.findByIdAndDelete(id, req.body);
+    return res.status(200).json(post);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const updatePost = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const post = await Post.findByIdAndUpdate(id, req.body);
+    await post.save();
+    return res.status(202).json(post);
+  } catch (err) {
+    return next(err);
+  }
+};
+
 exports.getAllPosts = getAllPosts;
 exports.createPost = createPost;
+exports.deletePost = deletePost;
+exports.updatePost = updatePost;
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,16 @@
 const express = require("express");
 
 const route = express.Router();
-const { getAllPosts, createPost } = require("../controllers/posts");
+const {
+  getAllPosts,
+  createPost,
+  deletePost,
+  updatePost,
+} = require("../controllers/posts");
 
 route.get("/", getAllPosts);
 route.post("/", createPost);
+route.delete("/:id", deletePost);
+route.put("/:id", updatePost);
 
 module.exports = route;
